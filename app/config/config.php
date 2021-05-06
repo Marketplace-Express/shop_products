b17b007b5ff48756184b3614f852f19ee7bc91a5
wajdi <creations.forum@gmail.com>
2021-05-06 21:06:22 +0300

    Update docker.yml

--- app/config/config.php
+++ app/config/config.php
@@ -18,6 +18,8 @@ $loader->registerFiles([
 $dotenv = Dotenv\Dotenv::createImmutable(BASE_PATH);
 $dotenv->load();
 
+exit($_ENV['MYSQL_HOST']);
+
 return new \Phalcon\Config([
     'database' => [
         'adapter' => 'Mysql',
