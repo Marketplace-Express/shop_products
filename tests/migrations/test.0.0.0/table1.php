5cabcfef7a57fd243769952c45ed5d36e3c65d0a
wajdi <creations.forum@gmail.com>
2021-05-08 22:43:20 +0300

    Update docker.yml

---
+++ tests/migrations/test.0.0.0/table1.php
@@ -0,0 +1,61 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class Table1Migration_11600
+ */
+class Table1Migration_11600 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('table1', [
+                'columns' => [
+                    new Column(
+                        'column_1',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 10,
+                            'first' => true
+                        ]
+                    )
+                ],
+                'options' => [
+                    'TABLE_TYPE' => 'BASE TABLE',
+                    'AUTO_INCREMENT' => '',
+                    'ENGINE' => 'InnoDB',
+                    'TABLE_COLLATION' => 'utf8mb4_0900_ai_ci'
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
--- utilities/docker-entrypoint.sh
+++ utilities/docker-entrypoint.sh
@@ -5,6 +5,10 @@ echo "Copying php extensions to container ..."
 cp -a php_extensions/. /usr/local/etc/php/conf.d/
 
 echo "Run migrations ..."
-phalcon migration --action=run --migrations=app/migrations/
+if [[ "${UNIT_TEST}" ]]; then
+  phalcon migration run --migrations=tests/migrations/
+else
+  phalcon migration run --migrations=app/migrations/
+fi
 
 exec "$@"
